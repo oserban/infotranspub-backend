1cb63d5d7985280ab302bdcf8867680e575e3941
Alexandru-Grosu <alexmgrosu@gmail.com>
2016-11-07 07:46:13 +0200

    Prima versiune a serverului REST

---
+++ rest/server/README.md
@@ -0,0 +1,4 @@
+Alfa este prima versiune a serverului REST. Dupa ce se stabilizeaza va deveni beta, si apoi stable. 
+
+Alfa -> Beta -> .. (acest folder)
+
---
+++ rest/server/alfa/src/main/java/ro/gov/ithub/Routes.java
@@ -0,0 +1,20 @@
+package ro.gov.ithub;
+
+import io.netty.handler.codec.http.HttpMethod;
+
+import org.restexpress.RestExpress;
+
+public abstract class Routes
+	{
+	public static void define(Configuration config, RestExpress server)
+    {
+		server.uri("/api/listaorase", config.getSampleController())
+			.action("readListaOrase", HttpMethod.GET)
+			.method(HttpMethod.POST)
+			.noSerialization()
+			.name(Constants.Routes.SAMPLE_COLLECTION);
+
+// or...
+//		server.regex("/some.regex", config.getRouteController());
+    }
+}
