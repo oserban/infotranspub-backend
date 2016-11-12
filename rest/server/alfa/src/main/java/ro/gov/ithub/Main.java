cfe2c24e4861c99a60e4d8fa284a72df23123f00
Mihnea <lzr_mihnea@yahoo.com>
2016-11-12 12:11:35 +0200

    [alfa] Application switched over to Spring Boot Application, with CityController set up as Spring REST Service

--- rest/server/alfa/src/main/java/ro/gov/ithub/Main.java
+++ rest/server/alfa/src/main/java/ro/gov/ithub/Main.java
@@ -1,13 +1,31 @@
 package ro.gov.ithub;
-import org.restexpress.util.Environment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class Main 
-{
-    public static void main( String[] args ) throws Exception
-    {
-		Configuration config = Environment.load(args, Configuration.class);
-                Server server = new Server(config);
-                server.start().awaitShutdown();
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Main {
+    public static void main( String[] args ) throws Exception {
+		SpringApplication.run(Main.class, args);
+    }
 
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatchServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/api/*");
+        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+        return registration;
     }
 }
+
--- rest/server/alfa/src/main/java/ro/gov/ithub/cities/CityController.java
+++ rest/server/alfa/src/main/java/ro/gov/ithub/cities/CityController.java
@@ -1,44 +1,27 @@
 package ro.gov.ithub.cities;
 
-import org.restexpress.Request;
-import org.restexpress.Response;
-import ro.gov.ithub.Oras;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ro.gov.ithub.base.BaseController;
-import ro.gov.ithub.delegate.SearchDelegate;
 
 import java.util.*;
 
+@RestController
 public class CityController extends BaseController<City> {
 
-	public CityController()
-	{
-		super();
+
+	@RequestMapping("/cities")
+	public List<City> getAllCities() {
+		return getHardcodedCityList();
 	}
 
-	public List<City> getAllCities(Request request, Response response)
-	{
-		//TODO: Your 'GET collection' logic here...
-		printOutRequest(request);
-
+	private List<City> getHardcodedCityList() {
 		List<City>  list = new ArrayList<>();
 		list.add(new City(1, "Bucuresti"));
 		list.add(new City(2, "Cluj-Napoca"));
 		list.add(new City(3, "Oradea"));
 		list.add(new City(4, "Iasi"));
 		list.add(new City(5, "Timisoara"));
-
 		return list;
-	}
-
-	private void printOutRequest(Request request) {
-		Map<String, String> hmap = request.getQueryStringMap();
-
-		Set set = hmap.entrySet();
-		Iterator iterator = set.iterator();
-		while(iterator.hasNext()) {
-			Map.Entry mentry = (Map.Entry)iterator.next();
-			System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-			System.out.println(mentry.getValue());
-		}
 	}
 }
