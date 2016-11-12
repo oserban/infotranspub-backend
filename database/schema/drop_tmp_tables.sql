52533a5ffd520d6c13b3ec1b76ffae695c03a80a
Alexandru-Grosu <alexmgrosu@gmail.com>
2016-11-13 01:18:23 +0200

    Punere la zi a scripturilor SQL/GTFS

---
+++ database/schema/drop_tmp_tables.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS  tmp_agency;
+DROP TABLE IF EXISTS  tmp_calendar;
+DROP TABLE IF EXISTS  tmp_calendar_dates;
+DROP TABLE IF EXISTS  tmp_fare_attributes;
+DROP TABLE IF EXISTS  tmp_fare_rules;
+DROP TABLE IF EXISTS  tmp_feed_info;
+DROP TABLE IF EXISTS  tmp_frequencies;
+DROP TABLE IF EXISTS  tmp_routes;
+DROP TABLE IF EXISTS  tmp_shapes;
+DROP TABLE IF EXISTS  tmp_stop_times;
+DROP TABLE IF EXISTS  tmp_stops;
+DROP TABLE IF EXISTS  tmp_transfers;
+DROP TABLE IF EXISTS  tmp_trips;
+
