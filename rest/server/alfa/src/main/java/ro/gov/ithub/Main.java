e40a26448a848faec42f81a18299216f99baf9f2
Mihnea <lzr_mihnea@yahoo.com>
2016-11-23 16:25:50 +0200

    [rest server][INFOTRANS-82] application.properties contains the access credentials to the schema of the app - this PostgreSQL schema must be created before - the username and password are the credentials used to access that schema - Configured Main to connect to the schema and map the Entities to the Tables

--- rest/server/alfa/src/main/java/ro/gov/ithub/Main.java
+++ rest/server/alfa/src/main/java/ro/gov/ithub/Main.java
@@ -1,15 +1,20 @@
 package ro.gov.ithub;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.DispatcherServlet;
+import ro.gov.ithub.base.BaseRepositoryImpl;
 
 @ComponentScan
 @EnableAutoConfiguration
+@EnableJpaRepositories(basePackages = "ro.gov.ithub", repositoryBaseClass = BaseRepositoryImpl.class)
 @SpringBootApplication
 public class Main {
     public static void main( String[] args ) throws Exception {
