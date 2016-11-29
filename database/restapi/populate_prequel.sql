b91ea95c35f788fa2db4f0453211eaae35702809
Alexandru-Grosu <alexmgrosu@gmail.com>
2016-11-30 01:36:05 +0200

    Again all the files

---
+++ database/restapi/populate_prequel.sql
@@ -0,0 +1,10 @@
+-- Link tables
+DELETE FROM agency_routes;
+DELETE FROM city_agencies;
+
+-- Entity tables
+DELETE FROM stop;
+DELETE FROM calendar;
+DELETE FROM route;
+DELETE FROM agency;
+DELETE FROM city;
