a0c2e6dba0a0b08de1f242c716aa10844951c2f2
Mihnea Lazar <lzr_mihnea@yahoo.com>
2016-11-29 19:00:20 +0200

    [rest server][INFOTRANS-112] Adapt routes to get data from DB

--- rest/server/alfa/src/main/java/ro/gov/ithub/city/CityServiceImpl.java
+++ rest/server/alfa/src/main/java/ro/gov/ithub/city/CityServiceImpl.java
@@ -20,32 +20,15 @@ public class CityServiceImpl implements CityService {
     @Autowired
     private AgencyService agencyService;
 
-    private static Map<String, City> ALL_CITIES;
-    static {
-        List<City> cities = new ArrayList<>();
-//        cities.add(new City("ro_bh_oradea"));
-//        cities.add(new City("ro_cj_clujnapoca"));
-//        cities.add(new City("ro_bucharest"));
-//        cities.add(new City("ro_tm_timisoara"));
-//        cities.add(new City("ro_is_iasi"));
-
-        for(City city : cities) {
-            ALL_CITIES.put(city.getCityUrl(), city);
-        }
-    }
-
     @Override
     public Collection<City> getCities() {
-        if(ALL_CITIES == null) {
-            return new ArrayList<>();
-        }
-        return ALL_CITIES.values();
+        return this.cityRepository.findAll();
     }
 
     @Override
     public City getCityWithAgencies(String cityName) {
 
-        for (City c: ALL_CITIES.values()) {
+        for (City c: cityRepository.findAll()) {
             if(StringUtils.equalsIgnoreCase(cityName, c.getCityUrl())) {
                 c.setAgencies(agencyService.getAgenciesForCity(c));
                 return c;
@@ -53,12 +36,6 @@ public class CityServiceImpl implements CityService {
         }
 
         return null;
-    }
-
-    @Override
-    @Transactional
-    public List<City> findAllCitiesFromDb() {
-        return cityRepository.findAll();
     }
 
 }
