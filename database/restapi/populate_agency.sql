4504fe37f7aed83ce17e7c0a14556fb9961c9fa3
Alexandru-Grosu <alexmgrosu@gmail.com>
2016-11-30 01:18:34 +0200

    City + Agency

---
+++ database/restapi/populate_agency.sql
@@ -0,0 +1,5 @@
+-- Agency
+INSERT INTO AGENCY( agency_id, agency_lang, agency_name, agency_phone, agency_timezone, agency_url, city_city_id)
+VALUES
+ (1,'RO','Regia Autonoma de Transport Bucuresti','+4021-336-56-50','Europe/Bucharest','http://ratb.ro',1)
+,(2,'RO','ETA SA Ramnicu Valcea','0350-412-490','Europe/Bucharest','http://www.eta-bus.ro/',1);
